7517606b0d232dce557cfe3fb05c148e9e6206d4
ziauddeen028-ZIA <ziauddeen028@gmail.com>
2026-05-16 19:53:52 +0530

    added FAQ & Gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -48,7 +48,7 @@ export default function Gallery() {
                 </div>
 
                 {/* Mobile View */}
-                <div className="grid grid-cols-2 gap-5 lg:hidden">
+                <div className="grid grid-cols-1 gap-5 lg:hidden">
                     {(showAll ? images : images.slice(0, 4)).map((img, index) => (
                         <div key={index} className="overflow-hidden rounded-3xl">
                             <img
